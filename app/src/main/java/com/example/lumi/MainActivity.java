8d10209d6b1e8079b6fd59115ae8607af2644349
Dev-109 <rajgor.deval@yahoo.com>
2019-11-19 17:59:00 -0500

    lumi

--- app/src/main/java/com/example/lumi/MainActivity.java
+++ app/src/main/java/com/example/lumi/MainActivity.java
@@ -4,20 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static Button login_button;
+    private Menu globalmenu;
+    private FirebaseAuth mAuth;
+    private TextView message;
+    private Button signin;
+    private Button register;
+    private Button signout;
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 
         login_button = findViewById(R.id.login_button);
         login_button.setOnClickListener(new View.OnClickListener() {
@@ -44,11 +61,28 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
 
     public void openMenu1(){
         Intent intent = new Intent(this, Menu1.class);
         startActivity(intent);
     }
+
+
+
+
 
 }
